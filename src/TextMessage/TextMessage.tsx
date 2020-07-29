efd9986fef7d75fafba19e3bf6281e309731ec66
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2020-07-30 00:45:41 +0800

    Setup storybook

--- src/TextMessage/TextMessage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Props } from "./TextMessage.types";
-
-const TextMessage: React.FC<Props> = ({ message }) => {
-  return <p>{message}</p>;
-};
-
-export default TextMessage;
--- src/TextMessage/TextMessage.types.ts
+++
@@ -1,3 +0,0 @@
-export type Props = {
-  message: string;
-};
